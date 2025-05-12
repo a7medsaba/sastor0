13480c3dcac6eb1de72336a5b220922e924cd2a0
a7medsaba <93777725+a7medsaba@users.noreply.github.com>
2025-05-12 16:42:53 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,12 +1,1 @@
-from fastapi import FastAPI
-import uvicorn
-
-app = FastAPI()
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
 print("✅ الملف main.py يعمل بشكل سليم")
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000)
-    
