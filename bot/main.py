83d509c77ab275285d8b670c93f2ac0730fd038a
a7medsaba <93777725+a7medsaba@users.noreply.github.com>
2025-05-12 06:56:30 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -10,5 +10,4 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-   uvicorn.run("bot.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("bot.main:app", host="0.0.0.0", port=port)
