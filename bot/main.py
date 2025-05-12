fba3b06ce8f6da7040e4676aee93fe29357d40c8
a7medsaba <93777725+a7medsaba@users.noreply.github.com>
2025-05-12 06:57:25 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,6 +1,5 @@
 # main.py
 from fastapi import FastAPI
-import os
 import uvicorn
 
 app = FastAPI()
@@ -10,4 +9,4 @@ def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    uvicorn.run("bot.main:app", host="0.0.0.0", port=port)
+    uvicorn.run("bot.main:app", host="0.0.0.0", port=8000)
