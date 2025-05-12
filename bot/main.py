5f75ec48f632c3ccb73332649b3044349a70ba61
a7medsaba <93777725+a7medsaba@users.noreply.github.com>
2025-05-12 06:38:16 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,14 @@
+# main.py
 from fastapi import FastAPI
+import os
 import uvicorn
 
 app = FastAPI()
 
 @app.get("/health")
-def health_check():
+def health():
     return {"status": "ok"}
 
 if __name__ == "__main__":
-    import os
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run(app, host="0.0.0.0", port=port)
